09b684a36c6c3f93e778a1b6b54121c92ea40b3d
Alexandre JANNOTTA <jannotta.alexandre@gmail.com>
2021-02-05 21:21:54 +0100

    add multi dc support for certificates

--- config/server.sh
+++ config/server.sh
@@ -2,7 +2,7 @@
 # generate certificate
 cd /consul/data
 rm *.pem
-consul tls cert create -server -node server-1 -dc paris -ca /consul/certs/consul-agent-ca.pem -key /consul/certs/consul-agent-ca-key.pem
+consul tls cert create -server -node server-1 -dc $1 -ca /consul/certs/consul-agent-ca.pem -key /consul/certs/consul-agent-ca-key.pem
 
 # startup
 consul agent -config-dir=/consul/config -data-dir=/consul/data
