af08695fe49622982c0bc1a0531f884c3f8098ec
Alexandre JANNOTTA <jannotta.alexandre@gmail.com>
2021-02-12 13:02:42 +0100

    Update for DNS

--- config/config.sh
+++ config/config.sh
@@ -22,3 +22,19 @@ consul acl token create \
     -policy-name 'client' \
     -accessor 'aaab1e0a-d91b-f689-5f71-33b1305c6d2c' \
     -secret '6a414ac2-850f-7921-2ba2-141ee456f966'
+
+consul acl policy create \
+    -http-addr 'http://server-1:8500' \
+    -token 'd8a5fa60-cc67-499b-d363-94128d44b2d1' \
+    -name 'dns' \
+    -rules 'node_prefix "" {
+  policy = "read"
+}
+service_prefix "" {
+  policy = "read"
+}
+'
+
+consul acl token update \
+    -id 00000000-0000-0000-0000-000000000002 \
+    -policy-name 'dns'
