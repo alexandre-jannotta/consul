aaead4d75d9a2bf6aa10ed0f1fc538f4540c96d8
Alexandre JANNOTTA <jannotta.alexandre@gmail.com>
2021-02-12 12:36:37 +0100

    Add query sample

---
+++ config/query.sh
@@ -0,0 +1,22 @@
+# create
+curl http://127.0.0.1:8500/v1/query \
+    --request POST \
+    --data @- << EOF
+{
+  "ID": "abb68427-44eb-d189-4223-8cf290497693",
+  "Name": "whoami",
+  "Service": {
+    "Service": "whoami",
+    "Failover": {
+      "Datacenters": ["seclin"]
+    }
+  }
+}
+EOF
+
+# list
+curl http://127.0.0.1:8500/v1/query
+# execute
+curl http://127.0.0.1:8500/v1/query/abb68427-44eb-d189-4223-8cf290497693/execute
+# delete
+curl http://127.0.0.1:8500/v1/query/abb68427-44eb-d189-4223-8cf290497693 --request DELETE
